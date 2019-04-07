59c90072e4df866cb1f04b923092ae584cc157c9
JP <JP@JP-PC>
2019-04-08 01:24:09 +0900

    final version

--- javastudyForBaekjoon/src/javastudyForBaekjoon/SugarDelivery.java
+++ javastudyForBaekjoon/src/javastudyForBaekjoon/SugarDelivery.java
@@ -5,37 +5,25 @@ import java.util.Scanner;
 public class SugarDelivery {
 	
 	public static void main(String[] args) {
-		
-		Scanner scan = new Scanner(System.in);
-		
+		Scanner scan = new Scanner(System.in);		
 		int finalCount = -1;
 		int inputNum = scan.nextInt();
+		int tCount = 0;	
 		
-		if(inputNum % 5 == 0) {			
-			finalCount = inputNum / 5;
-		}
-		else if((inputNum % 5) % 3 == 0) {
-			System.out.println("case 2");
-			finalCount = (inputNum / 5) + ((inputNum % 5) / 3);
-		}
-		else if(inputNum % 3 == 0){
-			System.out.println("case 3");
-			finalCount = inputNum / 3;
-		}
-		else {
-			int count = (inputNum / 5) -1;
-			System.out.println("case 4");
-			while(count > 0) {				
-				if((inputNum - (5*count)) % 3 == 0) {			
-					finalCount = count + ((inputNum - (5*count)) / 3);
-					break;
-				}
-				else {
-					count--;
-				}
+		while(true) {					
+			if(inputNum % 5 == 0 ) {	
+				finalCount = (inputNum / 5) + tCount;
+				break;			}
+			
+			if(inputNum < 3) {
+				break;
+			}
+			else
+			{
+				inputNum -= 3;
+				tCount++;
 			}			
 		}
-		
 		System.out.println(finalCount);
 	}
 }
