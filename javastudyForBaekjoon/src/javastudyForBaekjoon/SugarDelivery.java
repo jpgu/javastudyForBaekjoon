327d33d8953851b30f0b5d2978cf718c92c0bedc
JP <JP@JP-PC>
2019-04-08 01:00:42 +0900

    .

--- javastudyForBaekjoon/src/javastudyForBaekjoon/SugarDelivery.java
+++ javastudyForBaekjoon/src/javastudyForBaekjoon/SugarDelivery.java
@@ -1,5 +1,41 @@
 package javastudyForBaekjoon;
 
-public class SugarDelivery {
+import java.util.Scanner;
 
+public class SugarDelivery {
+	
+	public static void main(String[] args) {
+		
+		Scanner scan = new Scanner(System.in);
+		
+		int finalCount = -1;
+		int inputNum = scan.nextInt();
+		
+		if(inputNum % 5 == 0) {			
+			finalCount = inputNum / 5;
+		}
+		else if((inputNum % 5) % 3 == 0) {
+			System.out.println("case 2");
+			finalCount = (inputNum / 5) + ((inputNum % 5) / 3);
+		}
+		else if(inputNum % 3 == 0){
+			System.out.println("case 3");
+			finalCount = inputNum / 3;
+		}
+		else {
+			int count = (inputNum / 5) -1;		
+			System.out.println("case 4");
+			while(count > 0) {				
+				if((inputNum - (5*count)) % 3 == 0) {			
+					finalCount = count + ((inputNum - (5*count)) / 3);
+					break;
+				}
+				else {
+					count--;
+				}
+			}			
+		}
+		
+		System.out.println(finalCount);
+	}
 }
